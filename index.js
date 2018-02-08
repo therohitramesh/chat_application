ff88072157e9426dd998b3b19d2090cb41e8b139
Rohit Ramesh <rohitramesh47@gmail.com>
2018-02-08 21:45:59 +0530

    Install socket.io and write script to make a new connection using socket.io

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var socket = require('socket.io');
 
 //App Setup
 var app = express();
@@ -9,3 +10,10 @@ var server = app.listen(4000,function(){
 
 //Static files
 app.use(express.static('public'));
+
+//Socket Setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+  console.log('Made socket connection.', socket.id)
+});

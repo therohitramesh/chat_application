d2628b566a84ebe2a8c819c586ce251b70ef7bbf
Rohit Ramesh <rohitramesh47@gmail.com>
2018-02-09 01:41:39 +0530

    Include message broadcast and mention active user

--- public/chat.js
+++ public/chat.js
@@ -6,6 +6,7 @@ var message = document.getElementById('message');
 var handle = document.getElementById('handle');
 var output = document.getElementById('output');
 var btn = document.getElementById('send');
+var feedback = document.getElementById('feedback');
 
 //Emit events
 btn.addEventListener('click', function(){
@@ -15,7 +16,15 @@ btn.addEventListener('click', function(){
   });
 });
 
+message.addEventListener('keypress', function(){
+  socket.emit('typing', handle.value);
+});
+
 //Listen for events
 socket.on('chat', function(data){
   output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+  feedback.innerHTML += '<p><em>' + data + ' is typing...</p>';
 });
